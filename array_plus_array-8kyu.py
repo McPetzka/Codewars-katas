7545064c2046a42a25f8c6212adacf529ebeca23
McPetzka <59689916+McPetzka@users.noreply.github.com>
2020-09-27 19:05:03 +0200

    Create array_plus_array-8kyu.py
    Array plus array-8kyu

---
+++ array_plus_array-8kyu.py
@@ -0,0 +1,9 @@
+'''
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+
+'''
+
+def array_plus_array(arr1,arr2):
+    return sum([(a+b) for a, b in zip(arr1,arr2)])
