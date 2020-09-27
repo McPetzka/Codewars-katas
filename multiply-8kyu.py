bba2a9012b09b57674fdf7e89f0f7bafd63cca58
McPetzka <59689916+McPetzka@users.noreply.github.com>
2020-09-27 19:58:38 +0200

    Create multiply-8kyu.py
    Multiply-8kyu

---
+++ multiply-8kyu.py
@@ -0,0 +1,18 @@
+'''
+
+This code does not execute properly. Try to figure out why.
+
+import codewars_test as test
+# TODO Write tests
+import solution # or from solution import example
+
+# test.assert_equals(actual, expected, [optional] message)
+@test.describe("Example")
+def test_group():
+    @test.it("test case")
+    def test_case():
+        test.assert_equals(1 + 1, 2)
+'''
+
+def multiply(a, b):
+  a * b
